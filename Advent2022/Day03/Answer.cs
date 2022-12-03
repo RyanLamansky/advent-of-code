e1a5f3eee396249c44e75aba8692e4c19f712e8d
Ryan Lamansky <13633345+RyanLamansky@users.noreply.github.com>
2022-12-03 09:20:02 -0600

    Day 3 completed.

---
+++ Advent2022/Day03/Answer.cs
@@ -0,0 +1,36 @@
+﻿namespace Advent2022.Day03;
+
+internal static class Answer
+{
+    static int Value(char input) => input switch
+    {
+        >= 'a' and <= 'z' => input - 'a' + 1,
+        >= 'A' and <= 'Z' => input - 'A' + 27,
+        _ => throw new Exception(),
+    };
+
+    public static void Solve()
+    {
+        var totalValueOfCommon = 0;
+
+        foreach (var line in EmbeddedResource.EnumerateLines("Advent2022.Day03.input.txt"))
+        {
+            var compartment1 = line[..(line.Length / 2)];
+            var compartment2 = line[compartment1.Length..];
+            var commonToBoth = new string(compartment1.Intersect(compartment2).ToArray());
+            var valueOfCommon = commonToBoth.Sum(Value);
+            totalValueOfCommon += valueOfCommon;
+        }
+
+        Console.WriteLine($"Part 1: {totalValueOfCommon}");
+
+        totalValueOfCommon = 0;
+        foreach (var group in EmbeddedResource.EnumerateLines("Advent2022.Day03.input.txt").Chunk(3))
+        {
+            var commonToAll = group[0].Intersect(group[1]).Intersect(group[2]);
+            totalValueOfCommon += commonToAll.Sum(Value);
+        }
+
+        Console.WriteLine($"Part 2: {totalValueOfCommon}");
+    }
+}
---
+++ Advent2022/EmbeddedResource.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace Advent2022;
+
+internal static class EmbeddedResource
+{
+    public static IEnumerable<string> EnumerateLines(string name)
+    {
+        using var input = Assembly.GetExecutingAssembly().GetManifestResourceStream(name) ?? throw new Exception();
+        using var reader = new StreamReader(input);
+
+        string? line;
+        while ((line = reader.ReadLine()) is not null)
+            yield return line;
+    }
+}

f70e68f41d281e523ee08ed2a9d7f94bbb22d5c0
Ryan Lamansky <13633345+RyanLamansky@users.noreply.github.com>
2022-12-04 18:52:48 -0600

    Added support for puzzles that have 64-bit integer results.

--- Advent.Tests/PuzzleExtensions.cs
+++ Advent.Tests/PuzzleExtensions.cs
@@ -15,4 +15,18 @@ internal static class PuzzleExtensions
         Assert.AreEqual(samplePart2, puzzle.RunSamplePart2(), $"{callerName}.SamplePart2 failed.");
         Assert.AreEqual(inputPart2, puzzle.RunInputPart2(), $"{callerName}.InputPart2 failed.");
     }
+
+    public static void Test(
+        this IPuzzle64 puzzle,
+        long samplePart1,
+        long inputPart1,
+        long samplePart2,
+        long inputPart2,
+        [System.Runtime.CompilerServices.CallerMemberName] string? callerName = null)
+    {
+        Assert.AreEqual(samplePart1, puzzle.RunSamplePart1(), $"{callerName}.SamplePart1 failed.");
+        Assert.AreEqual(inputPart1, puzzle.RunInputPart1(), $"{callerName}.InputPart1 failed.");
+        Assert.AreEqual(samplePart2, puzzle.RunSamplePart2(), $"{callerName}.SamplePart2 failed.");
+        Assert.AreEqual(inputPart2, puzzle.RunInputPart2(), $"{callerName}.InputPart2 failed.");
+    }
 }
--- Advent/Program.cs
+++ Advent/Program.cs
@@ -1,12 +1,26 @@
 ﻿foreach (var puzzle in System.Reflection.Assembly.GetExecutingAssembly()
     .GetTypes()
-    .Where(type => type.GetInterfaces().Any(i => i == typeof(Advent.IPuzzle)))
+    .Where(type => type.GetInterfaces().Any(i => i == typeof(Advent.IPuzzle)) && !type.IsInterface)
     .OrderBy(type => type.FullName)
     .Select(Activator.CreateInstance)
     .OfType<Advent.IPuzzle>())
 {
     Console.Write(puzzle.GetType().Namespace![11..].Replace("Day", ""));
     Console.Write(": ");
+
+    if (puzzle is Advent.IPuzzle64 puzzle64)
+    {
+        Console.Write(puzzle64.RunSamplePart1());
+        Console.Write(", ");
+        Console.Write(puzzle64.RunInputPart1());
+        Console.Write(", ");
+        Console.Write(puzzle64.RunSamplePart2());
+        Console.Write(", ");
+        Console.Write(puzzle64.RunInputPart2());
+        Console.WriteLine();
+        continue;
+    }
+
     Console.Write(puzzle.RunSamplePart1());
     Console.Write(", ");
     Console.Write(puzzle.RunInputPart1());

7354ddefdae287b67fe48ee8c2e632a4a672c948
Ryan Lamansky <13633345+RyanLamansky@users.noreply.github.com>
2023-12-07 12:17:16 -0600

    2023 day 7 part 2, performance optimization to part 1.

--- Advent/Year2023/Day07/Answer.cs
+++ Advent/Year2023/Day07/Answer.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-
-namespace Advent.Year2023.Day07;
+﻿namespace Advent.Year2023.Day07;
 
 public sealed class Answer : IPuzzle<int>
 {
@@ -31,60 +29,64 @@ public sealed class Answer : IPuzzle<int>
         public override string ToString() => this.Value.ToString();
     }
 
-    readonly struct Hand(ReadOnlySpan<char> raw) : IComparable<Hand>, IEnumerable<Card>
+    public enum HandType
     {
-        public readonly Card
-            Card0 = new(raw[0]),
-            Card1 = new(raw[1]),
-            Card2 = new(raw[2]),
-            Card3 = new(raw[3]),
+        HighCard,
+        OnePair,
+        TwoPair,
+        ThreeOfAKind,
+        FullHouse,
+        FourOfAKind,
+        FiveOfAKind,
+    }
+
+    readonly struct Hand : IComparable<Hand>
+    {
+        public readonly Card Card0, Card1, Card2, Card3, Card4;
+        public readonly HandType Type;
+
+        public Hand(ReadOnlySpan<char> raw)
+        {
+            Card0 = new(raw[0]);
+            Card1 = new(raw[1]);
+            Card2 = new(raw[2]);
+            Card3 = new(raw[3]);
             Card4 = new(raw[4]);
 
-        public enum HandType
-        {
-            HighCard,
-            OnePair,
-            TwoPair,
-            ThreeOfAKind,
-            FullHouse,
-            FourOfAKind,
-            FiveOfAKind,
+            Type = ComputeType(Card0, Card1, Card2, Card3, Card4);
         }
 
-        public HandType Type
+        static HandType ComputeType(params Card[] cards)
         {
-            get
-            {
-                var counts = this
+            var counts = cards
                     .GroupBy(card => card)
                     .Select(group => group.Count())
                     .OrderByDescending(count => count)
                     .ToArray();
 
-                if (counts.Length == 1)
-                    return HandType.FiveOfAKind;
+            if (counts.Length == 1)
+                return HandType.FiveOfAKind;
 
-                if (counts[0] == 4)
-                    return HandType.FourOfAKind;
+            if (counts[0] == 4)
+                return HandType.FourOfAKind;
 
-                if (counts[0] == 3)
-                {
-                    if (counts[1] == 2)
-                        return HandType.FullHouse;
+            if (counts[0] == 3)
+            {
+                if (counts[1] == 2)
+                    return HandType.FullHouse;
 
-                    return HandType.ThreeOfAKind;
-                }
-
-                if (counts[0] == 2)
-                {
-                    if (counts[1] == 2)
-                        return HandType.TwoPair;
-
-                    return HandType.OnePair;
-                }
-
-                return HandType.HighCard;
+                return HandType.ThreeOfAKind;
             }
+
+            if (counts[0] == 2)
+            {
+                if (counts[1] == 2)
+                    return HandType.TwoPair;
+
+                return HandType.OnePair;
+            }
+
+            return HandType.HighCard;
         }
 
         public int CompareTo(Hand other)
@@ -109,18 +111,7 @@ public sealed class Answer : IPuzzle<int>
             return Card4.CompareTo(other.Card4);
         }
 
-        public IEnumerator<Card> GetEnumerator()
-        {
-            yield return Card0;
-            yield return Card1;
-            yield return Card2;
-            yield return Card3;
-            yield return Card4;
-        }
-
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-        public override string ToString() => string.Concat(this.Select(card => card.Value));
+        public override string ToString() => string.Concat(Card0.Value, Card1.Value, Card2.Value, Card3.Value, Card4.Value);
     }
 
     public int Part1(IEnumerable<string> input) => input
@@ -135,8 +126,120 @@ public sealed class Answer : IPuzzle<int>
         .Select((hand, index) => hand.Bid * (index + 1))
         .Sum();
 
-    public int Part2(IEnumerable<string> input)
+    readonly struct CardWithJoker(char value) : IComparable<CardWithJoker>
     {
-        return 0;
+        public readonly char Value = value;
+
+        public int Strength => Value switch
+        {
+            'J' => 0,
+            '2' => 1,
+            '3' => 2,
+            '4' => 3,
+            '5' => 4,
+            '6' => 5,
+            '7' => 6,
+            '8' => 7,
+            '9' => 8,
+            'T' => 9,
+            'Q' => 10,
+            'K' => 11,
+            'A' => 12,
+            _ => -1, // Should never happen, but removes a warning.
+        };
+
+        public int CompareTo(CardWithJoker other) => Strength.CompareTo(other.Strength);
+
+        public override string ToString() => this.Value.ToString();
     }
+
+    readonly struct HandWithJoker : IComparable<HandWithJoker>
+    {
+        public readonly CardWithJoker Card0, Card1, Card2, Card3, Card4;
+        public readonly HandType Type;
+
+        public HandWithJoker(ReadOnlySpan<char> raw)
+        {
+            Card0 = new(raw[0]);
+            Card1 = new(raw[1]);
+            Card2 = new(raw[2]);
+            Card3 = new(raw[3]);
+            Card4 = new(raw[4]);
+
+            Type = ComputeType(Card0, Card1, Card2, Card3, Card4);
+        }
+
+        static HandType ComputeType(params CardWithJoker[] cards)
+        {
+            var jacks = cards.Count(card => card.Value == 'J');
+            var counts = cards
+                .Where(card => card.Value != 'J')
+                .GroupBy(card => card)
+                .Select(group => group.Count())
+                .OrderByDescending(count => count)
+                .ToArray();
+
+            if (counts.Length <= 1)
+                return HandType.FiveOfAKind;
+
+            counts[0] += jacks;
+
+            if (counts[0] == 4)
+                return HandType.FourOfAKind;
+
+            if (counts[0] == 3)
+            {
+                if (counts[1] == 2)
+                    return HandType.FullHouse;
+
+                return HandType.ThreeOfAKind;
+            }
+
+            if (counts[0] == 2)
+            {
+                if (counts[1] == 2)
+                    return HandType.TwoPair;
+
+                return HandType.OnePair;
+            }
+
+            return HandType.HighCard;
+        }
+
+        public int CompareTo(HandWithJoker other)
+        {
+            int comparison;
+
+            if ((comparison = Type.CompareTo(other.Type)) != 0)
+                return comparison;
+
+            if ((comparison = Card0.CompareTo(other.Card0)) != 0)
+                return comparison;
+
+            if ((comparison = Card1.CompareTo(other.Card1)) != 0)
+                return comparison;
+
+            if ((comparison = Card2.CompareTo(other.Card2)) != 0)
+                return comparison;
+
+            if ((comparison = Card3.CompareTo(other.Card3)) != 0)
+                return comparison;
+
+            return Card4.CompareTo(other.Card4);
+        }
+
+        public override string ToString() => string.Concat(Card0.Value, Card1.Value, Card2.Value, Card3.Value, Card4.Value);
+    }
+
+    public int Part2(IEnumerable<string> input) => input
+        .Select(line =>
+        {
+            var hand = new HandWithJoker(line.AsSpan(0, 5));
+            var bid = int.Parse(line.AsSpan(6));
+
+            return (Hand: hand, Bid: bid);
+        })
+        .OrderBy(hand => hand)
+        .Select((hand, index) => hand.Bid * (index + 1))
+        .Sum();
 }
